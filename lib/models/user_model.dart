6060e839b386481b312c649294ae51f4a1df652b
Hassan Amjad <hassanamjad9529@gmail.com>
2025-05-13 10:03:23 +0500

    hh

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -17,3 +17,41 @@ class UserModel {
     required this.phoneNumber,
   });
 }
+class UserModel {
+  final String uid;
+  final String email;
+  final String name;
+  final String role;
+  final String designation;
+  final double salary;
+  final String phoneNumber;
+
+  UserModel({
+    required this.uid,
+    required this.email,
+    required this.name,
+    required this.role,
+    required this.designation,
+    required this.salary,
+    required this.phoneNumber,
+  });
+}
+class UserModel {
+  final String uid;
+  final String email;
+  final String name;
+  final String role;
+  final String designation;
+  final double salary;
+  final String phoneNumber;
+
+  UserModel({
+    required this.uid,
+    required this.email,
+    required this.name,
+    required this.role,
+    required this.designation,
+    required this.salary,
+    required this.phoneNumber,
+  });
+}
